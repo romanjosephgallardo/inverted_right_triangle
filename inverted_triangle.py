27e09bd344b7f2523811841b9e8250abfce00505
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 19:38:04 +0800

    Add pseudocode

---
+++ inverted_triangle.py
@@ -0,0 +1,17 @@
+'''
+Generate an inverted right triangle of height n.
+
+Sample output
+Enter the height of the triangle: 6
+******
+*****
+****
+***
+**
+*
+'''
+
+#pseudocode
+# Get the height of the triangle
+# Generate the triangle
+# Print the triangle
