cc805b72a3d4716fc2fb71fd81264832fd2ded5c
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 19:45:19 +0800

    Implement the for loop as a def function.

--- inverted_triangle.py
+++ inverted_triangle.py
@@ -1,8 +1,10 @@
 # Inverted triangle
 
+# Define the function to generate the triangle
+def inverted_triangle(height_value):
+    for height_value in range(height_n, 0, -1):
+        print("*" * height_value)
+
 # User input or the height of the triangle
 height_n = int(input("Enter the height of the triangle: "))
-
-# Generate and print the triangle
-for height_value in range(height_n, 0, -1):
-    print("*" * height_value)
+inverted_triangle(height_n)
