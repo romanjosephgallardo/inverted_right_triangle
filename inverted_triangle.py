7d474a3dc307e1087a7775a43eef7ab91d6a211c
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 19:41:22 +0800

    Implement for loop for printing triangle.

--- inverted_triangle.py
+++ inverted_triangle.py
@@ -13,5 +13,8 @@ Enter the height of the triangle: 6
 
 #pseudocode
 # Get the height of the triangle
+height_n = int(input("Enter the height of the triangle: "))
+
 # Generate the triangle
-# Print the triangle
+for height_value in range(height_n, 0, -1):
+    print("*" * height_value)
